c4e290ea67b4e8dd908a39a7132f3b9556d826bd
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2021-02-21 17:53:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,41 +1,17 @@
-let radius;
-let len;
 let angle;
-let anglev;
-let anglea;
-let bob;
-let start;
-let gravity;
 
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    radius = 50;
-    len = 600;
-    angle = PI / 8;
-    bob = createVector();
-    start = createVector(width / 2, 0);
-    anglev = 0;
-    anglea = 0;
-    gravity = 0.98;
+    angle = 0;
 }
 
 function draw() {
     background(21);
-    stroke(220);
-    strokeWeight(8);
-    fill(150);
-
-    let force = (sin(angle) * gravity) / len;
-    anglea = -force;
-
-    anglev += anglea;
-    angle += anglev;
-
-    anglev *= 0.999;
-
-    bob.x = len * sin(angle) + start.x;
-    bob.y = len * cos(angle) + start.y;
-
-    line(start.x, start.y, bob.x, bob.y); // the rigid string.
-    ellipse(bob.x, bob.y, radius * 2, radius * 2); // the bob.
+    translate(0, height / 2);
+    stroke(0, 100, 100);
+    strokeWeight(4);
+    for (let i = 0; i < width; i += 10) {
+        line(i, 0, i, sin(i * 0.019 - angle) * 100);
+    }
+    angle += 0.01;
 }
